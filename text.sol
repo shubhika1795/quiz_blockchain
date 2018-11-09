15a845f6da81723a22a0f0f5e7efe6c6581fa0fd
shubhika1795 <32219662+shubhika1795@users.noreply.github.com>
2018-11-09 11:02:01 +0530

    3rd commit

--- text.sol
+++ text.sol
@@ -5,6 +5,7 @@ contract TicTacToe{
         address public player1;
         address public player2;
         bool public flag=false;
+        int total_move;
         function TicTacToe()
         {
             player1 = msg.sender;
@@ -12,6 +13,7 @@ contract TicTacToe{
             {
                 board[i]=-1;
             }
+            total_move=0;
         }
         
         function register_player()
@@ -19,13 +21,50 @@ contract TicTacToe{
             player2=msg.sender;
             flag=true;
         }
-        function check_state() returns(uint)
+        function check_state(int val) returns(int)
         {
-            
+            if(board[0]==val && board[0]==board[1] && board[1]==board[2])
+            {
+                return val;
+            }
+            else if(board[3]==val && board[3]==board[4] && board[4]==board[5])
+            {
+                return val;
+            }
+            else if(board[6]==val && board[6]==board[7] && board[7]==board[8])
+            {
+                return val;
+            }
+            else if(board[0]==val && board[0]==board[3] && board[3]==board[6])
+            {
+                return val;
+            }
+            else if(board[1]==val && board[1]==board[4] && board[4]==board[7])
+            {
+                return val;
+            }
+            else if(board[2]==val && board[2]==board[5] && board[5]==board[8])
+            {
+                return val;
+            }
+            else if(board[0]==val && board[0]==board[4] && board[4]==board[8])
+            {
+                return val;
+            }
+            else if(board[2]==val && board[2]==board[4] && board[4]==board[6])
+            {
+                return val;
+            }
+            else
+            {
+                return 0;
+            }
         }
         function update_board(int val,uint place) returns(bool)
         {
             int x=board[place];
+            if(val!=0 || val!=1)
+                return false;
             if(x==-1 && flag==true)
             {
                 board[place]=val;
@@ -39,24 +78,28 @@ contract TicTacToe{
         {
             if(update_board(val,place))
             {
-                uint x=check_state();
+                total_move++;
+                int x=check_state(val);
                 if(x==0)
                 {
-                    
+                    if(total_move<9)
+                        return "continue";
+                    else
+                      return "Draw";
                 }
                 else if(x==1)
                 {
-                    
+                   return "Player 1 wins!";
                 }
                 else if(x==2)
                 {
-                    
+                    return "Player 2 wins!";
                 }
                  
                  
             }
             else
-                return "invalid";
+                return "invalid move";
         }
         
 
